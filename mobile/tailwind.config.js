ad728a4ccfe388e4d37c6bace8daa262ddd36407
ViniDevBR <vinigtr386@gmail.com>
2023-05-15 23:09:34 -0300

    :fire: Start mobile dev

---
+++ mobile/tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.tsx', './app/**/*.tsx'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
